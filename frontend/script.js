f65bae114d77492cddc9764d30d9216460e7aa15
GiovanniCalia <johnca@outlook.it>
2022-04-12 18:01:05 +0200

    Aggiornamento

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
-new vue({
+const app = new vue({
     el: "#app",
     created(){
-        axios.get("http://localhost:8888/?php-ajax-dischi/api.php").then(response => console.log(response.data)
+        axios.get("http://localhost:8888/php-ajax-dischi/api.php").then(response => console.log(response.data))
     }
 })
