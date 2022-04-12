c6330e7ccc92a0dc9e25af3eea5fb53fb8385c7c
GiovanniCalia <johnca@outlook.it>
2022-04-12 18:25:16 +0200

    Implementazione axios in vue e prova funzionamento

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,9 @@
-const app = new vue({
+const app = new Vue({
     el: "#app",
+    data: {
+        dischi: []
+    },
     created(){
-        axios.get("http://localhost:8888/php-ajax-dischi/api.php").then(response => console.log(response.data))
+        axios.get("http://localhost:8888/php-ajax-dischi/api.php").then(response => this.dischi = response.data)
     }
 })
