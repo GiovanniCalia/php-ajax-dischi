5d3d7a2096eafdfb4417d860dc2370d70abda156
GiovanniCalia <johnca@outlook.it>
2022-04-12 16:02:38 +0200

    Creazione header

--- parts/header.php
+++ parts/header.php
@@ -1,3 +1,10 @@
 <header>
-    <i class="fa-brands fa-spotify p-2 px-3 "></i>
+    <div><i class="fa-brands fa-spotify"></i></div>
+    <select class="select-header" name="genere">
+        <option value="">All</option>
+        <option value="Rock">Rock</option>
+        <option value="Pop">Pop</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Metal">Metal</option>
+    </select>
 </header>
